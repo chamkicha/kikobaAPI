3ec20756578d29471666e50605cfff00008aca84
fminja87 <fminja87@gmail.com>
2022-03-24 13:07:09 +0300

    logout api

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [AuthenticationApiController::class, 'logout']);
 });
 
 //API route for register new user
